59d29050bfbc387ea0a82bd775eb35c9ab3d50ed
wilson7016@gmail.com <hong95460>
2017-04-25 21:00:03 +0800

    Android-HW5-02

---
+++ app/src/main/java/Playoffs.java
@@ -0,0 +1,25 @@
+/**
+ * Created by user on 2017/4/25.
+ */
+
+public class Playoffs {
+    int hostImg;
+    String hostName;
+    String hostScore;
+
+    int guestImg;
+    String guestName;
+    String guestScore;
+
+    Playoffs(int hostImg, String hostName, String hostScore, int guestImg, String guestName, String guestScore){
+        hostImg = this.hostImg;
+        hostName = this.hostName;
+        hostScore = this.hostScore;
+
+        guestImg = this.guestImg;
+        guestName = this.guestName;
+        guestScore = this.guestScore;
+    }
+
+
+}
